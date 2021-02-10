6b6a585d5dc025a9affe9baabe849359b5cd8ed0
cristi070 <78821508+cristi070@users.noreply.github.com>
2021-02-10 22:21:17 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    speed = receivedNumber * SpeedMultiplier
+    basic.showNumber(receivedNumber)
+})
 radio.onReceivedString(function (receivedString) {
-    if (receivedString == "Open") {
-    	
-    } else if (receivedString == "Close") {
-    	
-    } else if (receivedString == "LED_GREEN") {
+    if (receivedString == "LED_GREEN") {
         DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBL, Color.GREEN)
         DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBR, Color.GREEN)
     } else if (receivedString == "LED_YELLOW") {
@@ -16,19 +16,22 @@ radio.onReceivedString(function (receivedString) {
         DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBL, Color.BLUE)
         DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBR, Color.BLUE)
     } else if (receivedString == "F") {
-        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 100)
+        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, speed)
     } else if (receivedString == "B") {
-        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CCW, 100)
+        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CCW, speed)
     } else if (receivedString == "L") {
-        DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 20)
-        DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 100)
+        DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CCW, speed/2)
+        DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, speed/2)
     } else if (receivedString == "R") {
-        DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 100)
-        DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 20)
+        DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, speed/2)
+        DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CCW, speed/2)
     } else {
         DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBL, Color.OFF)
         DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBR, Color.OFF)
         DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 0)
     }
 })
+let SpeedMultiplier = 20
+let speed = SpeedMultiplier
 radio.setGroup(1)
+basic.showNumber(1)
